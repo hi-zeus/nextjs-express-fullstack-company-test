dc1162670cb7ba69859f81f9d2f613075880f943
pro-top-star <topstar991122@gmail.com>
2023-04-22 18:39:10 +0900

    create initial project

---
+++ redux/store.js
@@ -0,0 +1,18 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./features/counterSlice";
+
+
+
+export const store = configureStore({
+  reducer: {
+    counterReducer,
+    // [userApi.reducerPath]: userApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({})
+});
+
+// setupListeners(store.dispatch);
+
+// export type RootState = ReturnType<typeof store.getState>;
+// export type AppDispatch = typeof store.dispatch;
